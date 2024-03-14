85e36f1baba28987fcc0493ce19ffad84ae75318
Rogelio Mendiola Jr <rjrmendiola@gmail.com>
2024-03-15 01:38:27 +0800

    Initial commit

---
+++ app/src/main/java/com/decoded/imagepoem/CameraActivity.kt
@@ -0,0 +1,260 @@
+package com.decoded.imagepoem
+
+import android.content.ContentValues
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCapture.OutputFileOptions
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.core.resolutionselector.AspectRatioStrategy
+import androidx.camera.core.resolutionselector.ResolutionSelector
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.decoded.imagepoem.databinding.ActivityCameraBinding
+import java.io.File
+import java.lang.Math.abs
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class CameraActivity : AppCompatActivity() {
+    private val cameraBinding: ActivityCameraBinding by lazy {
+        ActivityCameraBinding.inflate(layoutInflater)
+    }
+
+    private val multiplePermissionId = 14
+    private val multiplePermissionNameList = if (Build.VERSION.SDK_INT >= 33) {
+        arrayListOf(
+            android.Manifest.permission.CAMERA
+        )
+    } else {
+        arrayListOf(
+            android.Manifest.permission.CAMERA ,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+    }
+
+    private lateinit var imageCapture: ImageCapture
+    private lateinit var cameraProvider: ProcessCameraProvider
+    private lateinit var camera: Camera
+    private lateinit var cameraSelector: CameraSelector
+    private var lensFacing = CameraSelector.LENS_FACING_BACK
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(cameraBinding.root)
+
+        if(checkMultiplePermission()) {
+            startCamera()
+        }
+
+        cameraBinding.captureImage.setOnClickListener {
+            takePhoto()
+        }
+    }
+
+    private fun checkMultiplePermission(): Boolean {
+        val listPermissionNeeded = arrayListOf<String>()
+        for (permission in multiplePermissionNameList) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                listPermissionNeeded.add(permission)
+            }
+        }
+        if (listPermissionNeeded.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                this,
+                listPermissionNeeded.toTypedArray(),
+                multiplePermissionId
+            )
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == multiplePermissionId) {
+            if (grantResults.isNotEmpty()) {
+                var isGrant = true
+                for (element in grantResults) {
+                    if (element == PackageManager.PERMISSION_DENIED) {
+                        isGrant = false
+                    }
+                }
+                if (isGrant) {
+                    // here all permission granted successfully
+                    startCamera()
+                } else {
+                    var someDenied = false
+                    for (permission in permissions) {
+                        if (!ActivityCompat.shouldShowRequestPermissionRationale(
+                                this,
+                                permission
+                            )
+                        ) {
+                            if (ActivityCompat.checkSelfPermission(
+                                    this,
+                                    permission
+                                ) == PackageManager.PERMISSION_DENIED
+                            ) {
+                                someDenied = true
+                            }
+                        }
+                    }
+                    if (someDenied) {
+                        // here app Setting open because all permission is not granted
+                        // and permanent denied
+                        appSettingOpen(this)
+                    } else {
+                        // here warning permission show
+                        warningPermissionDialog(this) { _: DialogInterface, which: Int ->
+                            when (which) {
+                                DialogInterface.BUTTON_POSITIVE ->
+                                    checkMultiplePermission()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener({
+            cameraProvider = cameraProviderFuture.get()
+            bindCameraUserCases()
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+//    private fun setAspectRatio(ratio: String) {
+//        cameraBinding.previewView.layoutParams = cameraBinding.previewView.layoutParams.apply {
+//            if (this is ConstraintLayout.LayoutParams) {
+//                dimensionRatio = ratio
+//            }
+//        }
+//    }
+
+    private fun aspectRatio(width: Int, height: Int): Int {
+        val previewRatio = maxOf(width, height).toDouble() / minOf(width, height)
+        return if (abs(previewRatio - 4.0 / 3.0) <= abs(previewRatio - 16.0 / 9/0)) {
+            AspectRatio.RATIO_4_3
+        } else {
+            AspectRatio.RATIO_16_9
+        }
+    }
+
+    private fun bindCameraUserCases() {
+        val screenAspectRatio = aspectRatio(
+            cameraBinding.previewView.width,
+            cameraBinding.previewView.height
+        )
+        val rotation = cameraBinding.previewView.display.rotation
+
+        var resolutionSelector = ResolutionSelector.Builder()
+            .setAspectRatioStrategy(
+                AspectRatioStrategy(
+                    screenAspectRatio,
+                    AspectRatioStrategy.FALLBACK_RULE_AUTO
+                )
+            ).build()
+
+        val preview = Preview.Builder()
+            .setResolutionSelector(resolutionSelector)
+            .setTargetRotation(rotation)
+            .build()
+            .also {
+                it.setSurfaceProvider(cameraBinding.previewView.surfaceProvider)
+            }
+
+        imageCapture = ImageCapture.Builder()
+            .setCaptureMode(ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY)
+            .setResolutionSelector(resolutionSelector)
+            .setTargetRotation(rotation)
+            .build()
+
+        cameraSelector = CameraSelector.Builder()
+            .requireLensFacing(lensFacing)
+            .build()
+
+        try {
+            cameraProvider.unbindAll()
+
+            camera = cameraProvider.bindToLifecycle(
+                this, cameraSelector, preview, imageCapture
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun takePhoto() {
+        val imageFolder = File(
+            Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES
+            ), "Images"
+        )
+        if (!imageFolder.exists()) {
+            imageFolder.mkdir()
+        }
+
+        val fileName = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+            .format(System.currentTimeMillis()) + ".jpg"
+        val imageFile = File(imageFolder, fileName)
+        val outputOption = OutputFileOptions.Builder(imageFile).build()
+
+        val contentValues = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME,fileName)
+            put(MediaStore.Images.Media.MIME_TYPE,"image/jpeg")
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P){
+                put(MediaStore.Images.Media.RELATIVE_PATH,"Pictures/Images")
+            }
+        }
+
+        val metadata = ImageCapture.Metadata().apply {
+            isReversedHorizontal = (lensFacing == CameraSelector.LENS_FACING_FRONT)
+        }
+
+        imageCapture.takePicture(
+            outputOption,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val message = "Photo Capture Succeeded: ${outputFileResults.savedUri}"
+                    Toast.makeText(
+                        this@CameraActivity,
+                        message,
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Toast.makeText(
+                        this@CameraActivity,
+                        exception.message.toString(),
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        )
+    }
+}
